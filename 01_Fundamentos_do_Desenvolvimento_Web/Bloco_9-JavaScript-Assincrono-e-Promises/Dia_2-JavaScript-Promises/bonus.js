efe43c448e87a3d4c68e845184b109315bfd3b5a
Caio Lima <caiojlimah@gmail.com>
2021-09-16 18:53:13 -0300

    exercicio 9.2

---
+++ 01_Fundamentos_do_Desenvolvimento_Web/Bloco_9-JavaScript-Assincrono-e-Promises/Dia_2-JavaScript-Promises/bonus.js
@@ -0,0 +1,18 @@
+const makeRandomArray = async () => {
+  const array = [];
+  for (let i = 0; i < 10; i += 1) {
+    const number = Math.floor(Math.random() * 50 + 1);
+    array.push(number * number);
+  }
+  console.log(array.reduce((acc, elem) => acc + elem, 0))
+  if (array.reduce((acc, elem) => acc + elem, 0) < 8000 ) {
+    return array.reduce((acc, elem) => acc + elem, 0)
+  } else {
+    throw new Error();
+  }
+  
+}
+const mensagem = makeRandomArray()
+.then((resolve) => [resolve/2, resolve/3, resolve/5, resolve/10])
+.then((array) => console.log(array.reduce((acc, elem) => acc + elem), 0))
+.catch(() => console.log('É mais de oito mil! Essa promise deve estar quebrada!'))
---
+++ 01_Fundamentos_do_Desenvolvimento_Web/Bloco_9-JavaScript-Assincrono-e-Promises/Dia_2-JavaScript-Promises/exercicio2.js
@@ -0,0 +1,11 @@
+const promise = new Promise((resolve, reject) => {
+  const array = [];
+  for (let i = 0; i < 10; i += 1) {
+    const number = Math.floor(Math.random() * 50 + 1);
+    array.push(number * number);
+  }
+  (array.reduce((acc, elem) => acc + elem, 0) < 8000 ) ? resolve(array.reduce((acc, elem) => acc + elem, 0)) : reject()
+  
+}).then((resolve) => [resolve/2, resolve/3, resolve/5, resolve/10])
+.then((array) => array.reduce((acc, elem) => acc + elem), 0)
+.catch((reject) => console.log('É mais de oito mil! Essa promise deve estar quebrada!'))
